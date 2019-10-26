893e3fbc19ab28565d8bbf24e2f6ba8f6edbcf9b
James T <github.jtara1@gmail.com>
2019-10-26 13:17:50 -0700

    add: option to only use wasd for movement (for josh)

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -6,7 +6,9 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
     private CharacterController _controller;
-    
+
+    [SerializeField]
+    private bool wasdOnly = false;
     [SerializeField]
     public float Speed = 15f;
     
@@ -24,8 +26,17 @@ public class CharacterMovement : MonoBehaviour
 
     void Move()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        float horizontal, vertical;
+        if (!wasdOnly)
+        {
+            horizontal = Input.GetAxis("Horizontal");
+            vertical = Input.GetAxis("Vertical");
+        }
+        else
+        {
+            horizontal = Input.GetKey(KeyCode.D) ? 1 : 0 - (Input.GetKey(KeyCode.A) ? 1 : 0);
+            vertical = Input.GetKey(KeyCode.W) ? 1 : 0 - (Input.GetKey(KeyCode.S) ? 1 : 0);
+        }
         
         _controller.SimpleMove(Speed * new Vector3(horizontal, 0, vertical));
     }
