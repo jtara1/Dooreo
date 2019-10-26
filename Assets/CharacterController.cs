72474cb1d97e6a1429b19ee15568e5e90294482c
James T <github.jtara1@gmail.com>
2019-10-25 23:27:44 -0700

    add: char & movement controls

---
+++ Assets/CharacterController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    private UnityEngine.CharacterController controller;
+    
+    [SerializeField]
+    public float Speed = 15f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<UnityEngine.CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        
+        controller.SimpleMove(Speed * new Vector3(horizontal, 0, vertical));
+    }
+}
---
+++ CREDITS.md
@@ -0,0 +1,3 @@
+| author | name | url |
+| --- | --- | --- |
+| blacklodgegames | 36 Free Ground Textures | https://opengameart.org/content/36-free-ground-textures-diffuse-normals |
