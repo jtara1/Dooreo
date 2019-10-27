f6dccc2ebec5ff19303d95e3433dcf0aa1e2c700
James T <github.jtara1@gmail.com>
2019-10-26 18:43:13 -0700

    add: 3 EnemyManager (EM) scripts;

---
+++ Assets/Scripts/EnemyManager/EMMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EMMovement : MonoBehaviour
+{
+    private EnemyManager _enemyManager;
+    [SerializeField] private Transform transformToFollow;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _enemyManager = GetComponent<EnemyManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetTargetToMoveTo();
+    }
+
+    void SetTargetToMoveTo()
+    {
+        foreach (GameObject agent in _enemyManager.Enemies)
+        {
+            agent.GetComponent<NavMeshAgent>().SetDestination(transformToFollow.transform.position);
+        }
+    }
+}
