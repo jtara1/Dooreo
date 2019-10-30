498db29f835bab6b7d3932bd68933736c76a8a71
James T <github.jtara1@gmail.com>
2019-10-29 22:35:52 -0700

    add: scoreToWin and GameWon event

--- Assets/Scripts/GameManager/GMScore.cs
+++ Assets/Scripts/GameManager/GMScore.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GMScore : MonoBehaviour
 {
     private int _score = 0;
+    [SerializeField] private int scoreToWin = 100;
+    
+    public readonly UnityEvent GameWon = new UnityEvent();
 
     public int Score => _score;
 
     public void AddScore(GameObject enemy)
     {
         _score += enemy.GetComponent<Enemy>().ScoreValue;
+        if (Score >= scoreToWin) GameWon.Invoke();
     }
 }
