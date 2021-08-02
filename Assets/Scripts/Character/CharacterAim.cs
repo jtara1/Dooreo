8d232e2858ab698063c3358751e88065640eb36f
James T <github.jtara1@gmail.com>
2021-08-02 01:51:34 -0700

    fix(CharacterAim): corrected aim offset based for 16:9 aspect ratio (1:1 it was fine)

--- Assets/Scripts/Character/CharacterAim.cs
+++ Assets/Scripts/Character/CharacterAim.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CharacterAim : MonoBehaviour
 {
+    private static float ASPECT_RATIO_16_BY_9_BIAS = 15;
+    
     // Update is called once per frame
     void Update()
     {
@@ -14,21 +16,48 @@ public class CharacterAim : MonoBehaviour
     {
         Vector3 offset = new Vector3(0.5f, 0.5f);
         Vector3 viewportPoint = Camera.main.ScreenToViewportPoint(Input.mousePosition) - offset;
-        
+
         float angle = Vector3.SignedAngle(Vector3.right, viewportPoint, Vector3.up);
+        angle = this.AddAspectRatioDistortionCorrection(angle, viewportPoint.x < 0);
 
         if (viewportPoint.y > 0)
         {
             angle *= -1;
         }
+
         transform.rotation = Quaternion.Euler(new Vector3(0, angle, 0));
     }
 
-    /// <summary>
-    /// TODO: fix
-    /// Get the character position in viewport point (from perspective of camera)
-    /// </summary>
-    /// <returns></returns>
+    float AddAspectRatioDistortionCorrection(float degrees, bool addCorrection = false)
+    {
+        // equivalent angle in 1st quadrant
+        float referenceAngle;
+        if (degrees <= 90)
+        {
+            referenceAngle = degrees;
+        }
+        else if (degrees <= 180)
+        {
+            referenceAngle = degrees - 90;
+        }
+        else if (degrees <= 270)
+        {
+            referenceAngle = degrees - 180;
+        }
+        else
+        {
+            referenceAngle = degrees - 270;
+        }
+        
+        // f(x) = 1 - (1 / 2025) * (45 - x)^2
+        // roots at 0 and 90; parabola vertex at (45, 1)
+        float closenessTo45Degrees = 1 - Mathf.Pow(45 - referenceAngle, 2) / 2025;
+        closenessTo45Degrees = Mathf.Max(closenessTo45Degrees, 0);
+
+        float correction = CharacterAim.ASPECT_RATIO_16_BY_9_BIAS * closenessTo45Degrees;
+        return degrees + (addCorrection ? 1 : -1) * 15 * closenessTo45Degrees;
+    }
+    
     Vector3 GetCenter()
     {
         return Camera.main.WorldToViewportPoint(transform.position);
